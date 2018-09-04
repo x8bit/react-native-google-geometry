83b364375167121452656d110b2cce53c0bd5173
Ivan Martinez <ivan@x8bit.com>
2018-09-04 15:12:51 -0500

    implementation

--- android/src/main/java/com/varenslab/google/geometry/RNGoogleGeometryModule.java
+++ android/src/main/java/com/varenslab/google/geometry/RNGoogleGeometryModule.java
@@ -5,6 +5,12 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Promise;
 
 import com.google.maps.android.PolyUtil;
 import com.google.android.gms.maps.model.LatLng;
@@ -25,49 +31,77 @@ public class RNGoogleGeometryModule extends ReactContextBaseJavaModule {
     return "RNGoogleGeometry";
   }
 
-      @ReactMethod
-    public void containsLocation(
-        ReadableMap point,
-        ReadableArray polygon,
-        Callback completionCallback) {
+  // @ReactMethod
+  // public void containsLocation(
+  //     ReadableMap point,
+  //     ReadableArray polygon,
+  //     Callback completionCallback) {
 
-      LatLng locationPoint = new LatLng(
-          point.getDouble("lat"),
-          point.getDouble("lng")
-      );
+  //   LatLng locationPoint = new LatLng(
+  //       point.getDouble("lat"),
+  //       point.getDouble("lng")
+  //   );
 
-      List<LatLng> polygonList = new ArrayList<>();
+  //   List<LatLng> polygonList = new ArrayList<>();
 
-      for (int i = 0; i < polygon.size(); i++) {
-        ReadableMap vertex = polygon.getMap(i);
-        polygonList.add(
-            new LatLng(
-              vertex.getDouble("lat"),
-              vertex.getDouble("lng")
-            )
-        );
-      }
+  //   for (int i = 0; i < polygon.size(); i++) {
+  //     ReadableMap vertex = polygon.getMap(i);
+  //     polygonList.add(
+  //         new LatLng(
+  //           vertex.getDouble("lat"),
+  //           vertex.getDouble("lng")
+  //         )
+  //     );
+  //   }
 
-      boolean isWithinCoverage = PolyUtil.containsLocation(locationPoint, polygonList, false);
+  //   boolean isWithinCoverage = PolyUtil.containsLocation(locationPoint, polygonList, false);
 
-      completionCallback.invoke(isWithinCoverage);
-    }
+  //   completionCallback.invoke(isWithinCoverage);
+  // }
 
     // //isLocationOnPath(LatLng point, java.util.List<LatLng> polyline, boolean geodesic)
 
-    //       @ReactMethod
-    // public void isLocationOnPath(
-    //     ReadableMap point,
-    //     ReadableArray polyline,
-    //     Boolean geodesic,
-    //     Callback completionCallback) {
-
-    // }
-
-    //decode(java.lang.String encodedPath)
     @ReactMethod
-    public void decode(String encodedPath, Callback completionCallback) {
-      ReadableArray arr = PolyUtil.decode(encodedPath);
-      completionCallback.invoke(arr);
+    public void isLocationOnPath(
+        ReadableMap point,
+        ReadableArray polyline,
+        Boolean geodesic,
+        Double tolerance,
+        Promise promise) {
+
+        LatLng locationPoint = new LatLng(
+            point.getDouble("latitude"),
+            point.getDouble("longitude")
+        );
+
+        List<LatLng> pL = new ArrayList<>();
+
+        for (int i = 0; i < polyline.size(); i++) {
+          ReadableMap p = polyline.getMap(i);
+          pL.add(
+              new LatLng(
+                p.getDouble("latitude"),
+                p.getDouble("longitude")
+              )
+          );
+        }
+
+        Boolean onPath = PolyUtil.isLocationOnPath(locationPoint, pL, geodesic, tolerance);
+        promise.resolve(onPath);
+    }
+
+    @ReactMethod
+    public void decode(String encodedPath, Promise promise) {
+      List<LatLng> polyPoints = PolyUtil.decode(encodedPath);
+
+      WritableArray points = Arguments.createArray();
+      for(LatLng latlng: polyPoints){
+        WritableMap map = Arguments.createMap();
+        map.putDouble("latitude", latlng.latitude);
+        map.putDouble("longitude", latlng.longitude);
+        points.pushMap(map);
+      }
+
+      promise.resolve(points);
     }
 }
--- index.js
+++ index.js
@@ -3,18 +3,4 @@ import { NativeModules } from 'react-native';
 
 const { RNGoogleGeometry } = NativeModules;
 
-const containsLocation  = async (point,polygon)=>{
-    let isInLocation = await RNGoogleGeometry.containsLocation(point,polygon)
-    if(isInLocation){
-        return true
-    }else{
-        return false
-    }
-}
-
-const decode  = async (encodedPath)=>{
-    let polyLine = await RNGoogleGeometry.decode(encodedPath)
-    return polyLine;
-}
-
-export default { containsLocation, decode }
+export default RNGoogleGeometry;
